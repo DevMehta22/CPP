c8b35dfe33d95ac325cb6485d74551e70013a38c
DevMehta22 <mehtadev907@gmail.com>
2024-08-03 12:06:29 +0530

    commit 2.0

--- Oops4.cpp
+++ Oops4.cpp
@@ -1,3 +1,61 @@
 // Inheritence in detail
 
+#include<iostream>
+#include<string>
+using namespace std;
 
+class Man{
+    string _name;
+    int _age;
+    Man(){}
+
+    friend class Superman;
+
+protected:
+    Man(const string &name,const int & age)
+    :_name(name),_age(age){}
+    void run(){
+        cout<<"Man can run"<<endl;
+    }
+
+public:
+    void sayName() const;
+};
+
+void Man::sayName() const {
+    cout<<"My name is "<<_name <<" and age is "<<_age<<endl;
+};
+
+//superman
+
+class Superman : public Man{
+    bool flight;
+public:
+    Superman(string name) : Man(name,26){}
+    void run(){
+        cout<<"Superman can run at light speed at age of "<<_age<<endl;
+    }
+    
+};
+
+//spiderman
+
+class Spiderman : public Man{
+    bool web;
+public:
+    Spiderman(string name) : Man(name,19){}
+    void run(){
+        cout<<"Spiderman can run at super speed"<<endl;
+        }
+};
+
+int main(){
+    Superman superman("Clark");
+    Spiderman spiderman("Peter");
+    superman.sayName();
+    superman.run();
+    spiderman.sayName();
+    spiderman.run();
+    
+    return 0;
+}
